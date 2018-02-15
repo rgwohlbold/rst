21470014684244dd6d6ff56c6e542fd3d0adad06
Richard Wohlbold <rigewo02@gmail.com>
2018-02-15 14:50:21 -0600

    Added test cases

---
+++ test.py
@@ -0,0 +1,12 @@
+import unittest
+
+class TestBattleground(unittest.TestCase):
+
+    def test_init(self):
+        pass
+
+
+class TestRobot(unittest.TestCase):
+
+    def test_solve(self):
+        pass
