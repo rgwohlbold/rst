83102600a99529b4fe9e68d7cbc65fc8bc930678
Tenniel Miao <tennielmiao@gmail.com>
2018-02-15 17:19:42 -0600

    update get_view method

--- battleground.py
+++ battleground.py
@@ -1,5 +1,6 @@
 import copy
 
+
 class Battleground(object):
     def __init__(self, terrain, fog):
 
@@ -18,34 +19,39 @@ class Battleground(object):
         self.terrain = terrain
         self.fog = fog
 
-    def get_view(self, i=-1, j=-1, birdseye=True):
+    def get_view(self, i1=-1, j1=-1, interfere=True, birdseye=True):
         """
         get a view of the battleground for a robot at (i, j)
-        when (i, j) == (-1, -1) by default, return a view without the robot
-        :param i: coordinate on axis-0/vertical axis (0-indexed)
-        :param j: coordinate on axis-1/horizontal axis (0-indexed)
+        when (i1, j1) == (-1, -1) by default, return a view without the robot
+        :param i1: coordinate on axis-0/vertical axis (0-indexed)
+        :param j1: coordinate on axis-1/horizontal axis (0-indexed)
+        :param interfere: if True, the fog in the 3*3 square around (i, j) on the battleground will be removed
         :param birdseye: if True, everything not covered in fog on the battleground will be revealed;
          otherwise, only the 4 adjacent entries will be revealed, and everything else will be covered in fog.
         :return:
         """
 
         if birdseye:
-            view = copy.deepcopy(self.terrain)
+            view = copy.deepcopy(self.terrain)  # default view: omniscience
             for i in range(self.m):
                 for j in range(self.n):
                     if self.fog[i][j]:  # there is fog in the area
                         view[i][j] = 1
         else:  # birdseye disabled
-            view = [[1] * self.n for _ in range(self.m)]
+            view = [[1] * self.n for _ in range(self.m)]  # default view: ignorance
 
-        if i >= 0 and j >= 0:  # not default -> add information
-            view[i][j] = self.terrain[i][j]
-            if i - 1 >= 0:
-                view[i - 1][j] = self.terrain[i][j]  # up
-            if i + 1 < self.m:
-                view[i + 1][j] = self.terrain[i + 1][j]  # down
-            if j - 1 >= 0:
-                view[i][j - 1] = self.terrain[i][j - 1]  # left
-            if j + 1 < self.n:
-                view[i][j + 1] = self.terrain[i][j + 1]  # right
+        if i1 >= 0 and j1 >= 0:  # not default -> add information
+            # get a list of coordinates for valid surrounding blocks
+            neighbors = [(i, j) for j in range(max(j1-1, 0), min(j1+2, self.n)) for i in range(max(i1-1, 0), min(i1+2, self.m))]
+
+            # update the view matrix
+            for i, j in neighbors:
+                view[i][j] = self.terrain[i][j]
+
+            # update the fog matrix (only if interfere==True)
+            if interfere:
+                for i, j in neighbors:
+                    self.fog[i][j] = False  # clear the fog out of the way
+
         return view
+
