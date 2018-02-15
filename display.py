60eabefacd7feb4d233176965caf7f699830854e
FunkyQChicken <spencergrobinson@gmail.com>
2018-02-15 15:38:19 -0600

    added display function

---
+++ display.py
@@ -0,0 +1,30 @@
+from colorama import init
+from colorama import Fore, Back, Style 
+
+init()
+
+displaymap = {
+        # explored
+        0 : Fore.RESET + "_",
+        
+        # fog 
+        1 : Fore.RESET + "#",
+
+        # chasm
+        2 : Fore.RED    + "O", 
+        # mountains
+        3 : Fore.GREEN  + "^",
+        4 : Fore.BLUE   + "^",
+        5 : Fore.CYAN   + "^",
+        6 : Fore.YELLOW + "^",
+        7 : Fore.GREEN  + "+",
+        8 : Fore.BLUE   + "+",
+        9 : Fore.CYAN   + "+"
+}
+
+def display(environment):
+    for row in environment:
+        for tile in row:
+            print(displaymap[tile],end=" ")
+        print()
+    print(Fore.RESET)
