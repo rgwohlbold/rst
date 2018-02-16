edc07528d1ce1df4376e0f954d94a18699f4c237
Richard Wohlbold <rigewo02@gmail.com>
2018-02-15 18:27:58 -0600

    PEP compliance in display.py

--- display.py
+++ display.py
@@ -1,29 +1,31 @@
 from colorama import init
-from colorama import Fore, Back, Style 
+from colorama import Fore, Back, Style
 
 init()
 
 displaymap = {
-        # explored
-        0 : Fore.RESET + "_",
-        
-        # fog 
-        1 : Fore.RESET + "#",
+    # explored
+    0: Fore.RESET + "_",
 
-        # chasm
-        2 : Fore.RED    + "O", 
-        # mountains
-        3 : Fore.GREEN  + "^",
-        4 : Fore.BLUE   + "^",
-        5 : Fore.CYAN   + "^",
-        6 : Fore.YELLOW + "^",
-        7 : Fore.GREEN  + "+",
-        8 : Fore.BLUE   + "+",
-        9 : Fore.CYAN   + "+"
+    # fog
+    1: Fore.RESET + "#",
+
+    # chasm
+    2: Fore.RED    + "O",
+
+    # mountains
+    3: Fore.GREEN  + "^",
+    4: Fore.BLUE   + "^",
+    5: Fore.CYAN   + "^",
+    6: Fore.YELLOW + "^",
+    7: Fore.GREEN  + "+",
+    8: Fore.BLUE   + "+",
+    9: Fore.CYAN   + "+"
 }
 
-def display(environment):
-    for row in environment:
+
+def display(terrain):
+    for row in terrain:
         for tile in row:
             print(displaymap[tile],end=" ")
         print()
