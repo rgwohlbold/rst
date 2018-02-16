e27618f11d276c9746023476b697e8bce913c503
Richard Wohlbold <rigewo02@gmail.com>
2018-02-15 19:56:16 -0600

    Added test for hull.py

--- test.py
+++ test.py
@@ -1,15 +1,18 @@
 import unittest
+from hull import graham_scan
 
-class TestBattleground(unittest.TestCase):
+class TestEverything(unittest.TestCase):
 
     def test_init(self):
         pass
 
+    def test_convex_hull(self):
+        points = [(0, -2), (0, 0), (-1, 0), (1, 2), (3, 0), (1, 5)]
+        res = graham_scan(points)
+        self.assertEquals(res, [(0, -2), (3, 0), (1, 5), (-1, 0)])
 
-class TestRobot(unittest.TestCase):
 
-    def test_solve(self):
-        pass
+
 
 
 if __name__ == "__main__":
