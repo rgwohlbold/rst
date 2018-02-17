9ea6c6de30db057bccc79388ec4eb3b0960319a5
Richard Wohlbold <rigewo02@gmail.com>
2018-02-16 19:37:37 -0600

    Added rubber band interpolation

--- src/test.py
+++ src/test.py
@@ -1,5 +1,6 @@
 import unittest
 from hull import graham_scan
+from interpolate import interpolate
 
 class TestEverything(unittest.TestCase):
 
@@ -9,11 +10,23 @@ class TestEverything(unittest.TestCase):
     def test_convex_hull(self):
         points = [(0, -2), (0, 0), (-1, 0), (1, 2), (3, 0), (1, 5)]
         res = graham_scan(points)
-        self.assertEquals(res, [(0, -2), (3, 0), (1, 5), (-1, 0)])
+        self.assertEqual(res, [(0, -2), (3, 0), (1, 5), (-1, 0)])
 
+    def test_interpolate_1(self):
+        points = [(0, -2), (3, 0), (1, 5), (-1, 0)]
+        self.assertEqual(interpolate(points),
+                         [(( 0.5, -2.5), ( 3.5, -0.5)),
+                          (( 3.5,  0.5), ( 1.5,  5.5)),
+                          (( 0.5,  5.5), (-1.5,  0.5)),
+                          ((-1.5, -0.5), (-0.5, -2.5))])
 
-
-
+    def test_interpolate_2(self):
+        points = [(0, -2), (2, 0), (0, 2), (-2, 0)]
+        self.assertEqual(interpolate(points),
+                         [(( 0.5, -2.5), ( 2.5, -0.5)),
+                          (( 2.5,  0.5), ( 0.5,  2.5)),
+                          ((-0.5,  2.5), (-2.5,  0.5)),
+                          ((-2.5, -0.5), (-0.5, -2.5))])
 
 if __name__ == "__main__":
     unittest.main()
