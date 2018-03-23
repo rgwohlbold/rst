5f4700e25f09c96b9055966511d7991783f7297f
Richard Wohlbold <rigewo02@gmail.com>
2018-03-23 08:51:07 -0500

    Integrated display into battleground

--- src/battleground.py
+++ src/battleground.py
@@ -1,6 +1,30 @@
 import copy
 from convex_hull import get_all_vertices, graham_scan
 from polygon import interpolate
+from colorama import init
+from colorama import Fore, Back, Style
+
+init()
+
+displaymap = {
+    # explored
+    0: Fore.RESET + "_",
+
+    # fog
+    1: Fore.RESET + "#",
+
+    # chasm
+    2: Fore.RED    + "O",
+
+    # mountains
+    3: Fore.GREEN  + "^",
+    4: Fore.BLUE   + "^",
+    5: Fore.CYAN   + "^",
+    6: Fore.YELLOW + "^",
+    7: Fore.GREEN  + "+",
+    8: Fore.BLUE   + "+",
+    9: Fore.CYAN   + "+"
+}
 
 DEBUG = False
 
@@ -68,15 +92,22 @@ class Battleground(object):
         self.fog = fog
 
     def __str__(self):
-        ret = "<RST Battleground Object: Battleground(\n"
-        ret += "terrain=[\n"
+        ret = ""
         for row in self.terrain:
-            ret += str(row) + ",\n"
-        ret = ret[:-2] + "],\nfog=[\n"
-        for row in self.fog:
-            ret += str(row) + ",\n"
-        ret = ret[:-2] + "])>"
+            for tile in row:
+                ret += displaymap[tile] + " "
+            ret += "\n"
+        ret += Fore.RESET
         return ret
+        # ret = "<RST Battleground Object: Battleground(\n"
+        # ret += "terrain=[\n"
+        # for row in self.terrain:
+        #     ret += str(row) + ",\n"
+        # ret = ret[:-2] + "],\nfog=[\n"
+        # for row in self.fog:
+        #     ret += str(row) + ",\n"
+        # ret = ret[:-2] + "])>"
+        # return ret
 
     def get_view(self, i1=-1, j1=-1, interfere=False, birdseye=True):
         """
