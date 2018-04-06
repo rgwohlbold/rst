469734d77ce62146944a99c91b617e12cdeb3225
FunkyQChicken <spencergrobinson@gmail.com>
2018-04-06 11:33:55 -0500

    added driver.py to display dfs

---
+++ src/driver.py
@@ -0,0 +1,17 @@
+from robot  import Robot
+from display import display 
+from battleground import Battleground
+view = [
+    [1, 1, 0, 1, 1, 1, 1, 1, 0],
+    [0, 1, 4, 1, 2, 4, 1, 1, 1],
+    [1, 1, 1, 2, 1, 1, 4, 1, 1],
+    [1, 2, 2, 4, 1, 1, 1, 1, 2],
+    [1, 1, 1, 1, 3, 1, 1, 1, 2],
+    [0, 1, 1, 1, 1, 1, 3, 1, 1],
+    [0, 1, 3, 1, 1, 2, 1, 1, 1],
+    [1, 0, 2, 0, 1, 2, 1, 1, 1]]
+
+ground = Battleground(view)
+print(ground)
+robot  = Robot(ground)
+robot.dfs(True)
