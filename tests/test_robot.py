ff6bdf25f97861a3ba02e0a5b81f77e0ad721dc1
Richard Wohlbold <rigewo02@gmail.com>
2018-05-04 09:54:08 -0500

    Added test tables in test_robot.py

--- tests/test_robot.py
+++ tests/test_robot.py
@@ -5,15 +5,52 @@ from rst.robot import Robot
 
 class TestRobot(unittest.TestCase):
 
-    def test_robot(self):
-        terrain = [[0,0],
-                   [0,0]]
-        battleground = Battleground(terrain=terrain)
-        robot = Robot(battleground)
-        self.assertTrue(robot.dfs())
-        self.assertEqual(robot.moves, 2)
+    small_terrain = [
+        [0,0],
+        [0,0]
+    ]
 
-        robot = Robot(battleground)
-        self.assertTrue(robot.follow_left())
-        self.assertTrue(robot.moves, 2)
+    empty_terrain = [
+        [0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0]
+    ]
+
+    test_table = [
+        {
+            "terrain": small_terrain,
+            "moves_left": 2,
+            "moves_right": 2,
+            "solvable": True
+        },
+        {
+            "terrain": empty_terrain,
+            "moves_left": 12,
+            "moves_right": 12,
+            "solvable": True
+        }
+
+    ]
+
+    def test_robot(self):
+
+        for entry in TestRobot.test_table:
+            battleground = Battleground(terrain=entry["terrain"])
+            robot = Robot(battleground)
+            self.assertEqual(robot.dfs(), entry["solvable"])
+
+            self.assertEqual(robot.dfs(), entry["solvable"])
+            battleground = Battleground(terrain=entry["terrain"])
+            robot = Robot(battleground)
+            self.assertEqual(robot.follow_right(), entry["solvable"])
+            self.assertEqual(robot.moves, entry["moves_left"])
+
+            battleground = Battleground(terrain=entry["terrain"])
+            robot = Robot(battleground)
+            self.assertEqual(robot.follow_right(), entry["solvable"])
+            self.assertEqual(robot.moves, entry["moves_right"])
 
