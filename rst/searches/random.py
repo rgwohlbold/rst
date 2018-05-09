d16902c1bb526d25e795e1f675efa149b7ab786c
FunkyQChicken <spencergrobinson@gmail.com>
2018-05-09 17:40:06 -0500

    added a Random Search

---
+++ rst/searches/random.py
@@ -0,0 +1,19 @@
+from searches.search import Search
+from random import randint
+
+class Random(Search):    
+    def tick(self):
+        directions = [(0,1), (1,0), (-1,0), (0,-1)]
+        direction = directions[randint(0,3)]
+        new_x, new_y = map(sum, zip(direction, (self.rob.x, self.rob.y)))
+        
+        view = self.rob.get_view()
+
+        if self.rob.battleground.in_bounds(new_x, new_y) and view[new_x][new_y] == 0:
+            self.inc_moves()
+            self.rob.x, self.rob.y = new_x, new_y
+        else:
+            return self.tick()
+        
+        return True
+        
